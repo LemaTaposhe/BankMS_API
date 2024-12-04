2eebb7cd6455d6a6f036385e9cf7ee0ef9e38a97
Lee <155739838+LemaTaposhe@users.noreply.github.com>
2024-12-04 19:44:58 +0600

    Update Program.cs
    done

--- Program.cs
+++ Program.cs
@@ -12,7 +12,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Configure Swagger with JWT authentication
+//builder.Services.AddSwaggerGen(swagger =>
+//{
+//    //swagger.SwaggerDoc("v1", new OpenApiInfo
+//    //{
+//    //    Version = "v1",
+//    //    Title = "JWT Token Authentication API",
+//    //    Description = ".NET 8 Web API"
+//    //});
+//    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+//    {
+//        Name = "Authorization",
+//        Type = SecuritySchemeType.ApiKey,
+//        Scheme = "Bearer",
+//        BearerFormat = "JWT",
+//        In = ParameterLocation.Header,
+//        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+//    });
+//    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+//    {
+//        {
+//            new OpenApiSecurityScheme
+//            {
+//                Reference = new OpenApiReference
+//                {
+//                    Type = ReferenceType.SecurityScheme,
+//                    Id = "Bearer"
+//                }
+//            },
+//            Array.Empty<string>()
+//        }
+//    });
+//});
 // Configure the database context to use SQL Server
 builder.Services.AddDbContext<BankDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbCon")));
