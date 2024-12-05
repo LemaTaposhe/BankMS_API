a813cbcc826ce5e0fc8acee9ca7745866639276f
Lee <155739838+LemaTaposhe@users.noreply.github.com>
2024-12-05 23:43:34 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using BankMS_API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -11,40 +12,33 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Configure Swagger with JWT authentication
-//builder.Services.AddSwaggerGen(swagger =>
-//{
-//    //swagger.SwaggerDoc("v1", new OpenApiInfo
-//    //{
-//    //    Version = "v1",
-//    //    Title = "JWT Token Authentication API",
-//    //    Description = ".NET 8 Web API"
-//    //});
-//    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-//    {
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer",
-//        BearerFormat = "JWT",
-//        In = ParameterLocation.Header,
-//        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-//    });
-//    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-//    {
-//        {
-//            new OpenApiSecurityScheme
-//            {
-//                Reference = new OpenApiReference
-//                {
-//                    Type = ReferenceType.SecurityScheme,
-//                    Id = "Bearer"
-//                }
-//            },
-//            Array.Empty<string>()
-//        }
-//    });
-//});
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
 // Configure the database context to use SQL Server
 builder.Services.AddDbContext<BankDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbCon")));
@@ -72,6 +66,7 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("EmployeeOnly", policy => policy.RequireRole("BankEmployee"));
     options.AddPolicy("CustomerOnly", policy => policy.RequireRole("Customer"));
 });
+
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
@@ -79,13 +74,15 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 
 // Optionally, add a Hosted Service (e.g., for interest calculation)
 builder.Services.AddHostedService<InterestCalculationService>();
+
 // Configure CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowOrigin", policy =>
         policy.WithOrigins("http://localhost:4200")
               .AllowAnyHeader()
-              .AllowAnyMethod());
+              .AllowAnyMethod()
+              .AllowCredentials());
 });
 
 var app = builder.Build();
@@ -108,9 +105,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseDeveloperExceptionPage(); // For development environment
-app.UseHttpsRedirection();
+
+// Remove HTTPS redirection to allow HTTP only
+// app.UseHttpsRedirection();
+
+app.UseRouting();
+
 // Apply CORS policy
 app.UseCors("AllowOrigin");
+
 // Add middleware for JWT authentication and authorization
 app.UseAuthentication();
 app.UseAuthorization();
@@ -120,4 +123,3 @@ app.MapControllers();
 
 // Run the application
 app.Run();
-
